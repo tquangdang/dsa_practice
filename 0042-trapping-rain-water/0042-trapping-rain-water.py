7f245ade4a4ccaa72854f97371f648cc56f60756
tquangdang <thanhquangdang2002@gmail.com>
2026-06-20 20:57:32 -0500

    Time: 3 ms (95.11%), Space: 21 MB (83.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,30 +1,21 @@
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # two pointers at beginning and end of array
-        left = 0
-        right = len(height) - 1
-		
-        higher_level = 0 # keep track of "highest" level of water seen so far
-        water = 0 # total water
-		
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        leftMax = rightMax = 0
+        water = 0
+
         while left < right:
-            if height[left] <= height[right]:
-		# if left pointer has lower level, store the value and move it one step to right
-                lower_level = height[left]
+            if height[left] < height[right]:
+                if height[left] >= leftMax:
+                    leftMax = height[left]
+                else:
+                    water += leftMax - height[left]
                 left += 1
             else:
-		# if right pointer has lower level, store the value and move it one step to left
-                lower_level = height[right]
+                if height[right] >= rightMax:
+                    rightMax = height[right]
+                else:
+                    water += rightMax - height[right]
                 right -= 1
-				
-	    # make sure to store and continuously update the current "highest" level of water
-            higher_level = max(higher_level, lower_level)
-			
-            # incrementally keep track of amount of water so far
-            water += higher_level - lower_level
-			
+
         return water
