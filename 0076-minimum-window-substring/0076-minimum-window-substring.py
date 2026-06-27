857a77930dddd40027e11f53cf5871aad044c510
tquangdang <thanhquangdang2002@gmail.com>
2026-06-26 21:28:21 -0500

    Time: 159 ms (18.63%), Space: 19.8 MB (36.75%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,43 @@
-'''
-A valid window should be:
-- Start with a char in s
-- End with a char in s'
-- Minimum length
-
-'''
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         t_freq = Counter(t)
-        have, need = 0, len(set(t)) 
+
+        have = 0
+        need = len(t_freq)
+
         s_freq = Counter()
+
         result = ""
-        left, right = 0, 0
+
+        left = 0
+        right = 0
+
         while right < len(s):
+
             if s[right] in t_freq:
                 s_freq[s[right]] += 1
-                # When should have increase?
-                # Hint: Only when s_freq[char] FIRST reaches t_freq[char]
+
                 if s_freq[s[right]] == t_freq[s[right]]:
                     have += 1
+
+            # Current window has everything needed.
             while have == need:
-                if right - left + 1 < len(result) or result == "":
+
+                # Only update result if this window is better.
+                if result == "" or right - left + 1 < len(result):
                     result = s[left:right + 1]
-                # Shrinking the window
+
+                # But ALWAYS shrink the window.
                 if s[left] in t_freq:
+
+                    # Removing this character will make the window invalid.
                     if s_freq[s[left]] == t_freq[s[left]]:
                         have -= 1
+
                     s_freq[s[left]] -= 1
+
                 left += 1
-                while left < right and s[left] not in t_freq:
-                    left += 1
+
             right += 1
+
         return result
